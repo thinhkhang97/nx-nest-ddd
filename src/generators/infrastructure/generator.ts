e9c70522cf93cc0b9b6eea1cd89bf54139b121c4
Khang Nguyễn <thinhkhang97@gmail.com>
2023-12-21 14:14:27 +0700

    feat: :sparkles: handle infrastructure layer

--- src/generators/infrastructure/generator.ts
+++ src/generators/infrastructure/generator.ts
@@ -1,24 +1,57 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { libraryGenerator } from '@nx/nest';
 import * as path from 'path';
+import { hyphenToCapital } from '../../utils';
+import infrastructureOrmEntityGenerator from './orm-entity/generator';
+import infrastructureOrmMapperGenerator from './orm-mapper/generator';
+import infrastructureRepositoryGenerator from './repository/generator';
 import { InfrastructureGeneratorSchema } from './schema';
 
 export async function infrastructureGenerator(
   tree: Tree,
   options: InfrastructureGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  const { name, tags, templatePath } = options;
+  await libraryGenerator(tree, {
+    ...options,
+    name: `${name}-application`,
+    directory: `libs/${name}/application`,
+    projectNameAndRootFormat: 'as-provided',
+    tags: tags ? `layer:application,${tags}` : `layer:application`,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  tree.delete(`libs/${name}/application/src/lib`);
+  tree.write(
+    `libs/${name}/application/src/index.ts`,
+    `export * from './modules/${name}-application.module';`
+  );
+  await infrastructureOrmEntityGenerator(tree, {
+    name: `${name}`,
+    subDomain: name,
+    skipFormat: true,
+    templatePath: templatePath && `${templatePath}/orm-entities`,
+  });
+  await infrastructureOrmMapperGenerator(tree, {
+    name: `${name}`,
+    subDomain: name,
+    skipFormat: true,
+    templatePath: templatePath && `${templatePath}/orm-mappers`,
+  });
+  await infrastructureRepositoryGenerator(tree, {
+    name: `${name}`,
+    subDomain: name,
+    skipFormat: true,
+    templatePath: templatePath && `${templatePath}/repositories`,
+  });
+  generateFiles(
+    tree,
+    (templatePath && `${templatePath}/modules`) ||
+      path.join(__dirname, 'files/modules'),
+    `libs/${name}/infrastructure/src/modules`,
+    {
+      name,
+      hyphenToCapital,
+    }
+  );
   await formatFiles(tree);
 }
 
--- src/generators/infrastructure/schema.d.ts
+++ src/generators/infrastructure/schema.d.ts
@@ -1,3 +1,26 @@
 export interface InfrastructureGeneratorSchema {
   name: string;
+  directory?: string;
+  importPath?: string;
+  publishable?: boolean;
+  skipFormat?: boolean;
+  skipTsConfig?: boolean;
+  strict?: boolean;
+  tags?: string;
+  target?:
+    | 'es5'
+    | 'es6'
+    | 'esnext'
+    | 'es2015'
+    | 'es2016'
+    | 'es2017'
+    | 'es2018'
+    | 'es2019'
+    | 'es2020'
+    | 'es2021';
+  testEnvironment?: 'jsdom' | 'node';
+  setParserOptionsProject?: boolean;
+  skipPackageJson?: boolean;
+  simpleName?: boolean;
+  templatePath?: string;
 }
