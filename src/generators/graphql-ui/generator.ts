189ee2e058c90f68ef0b32e2b867dc329653fc1f
Khang Nguyễn <thinhkhang97@gmail.com>
2023-12-28 14:46:58 +0700

    feat: :sparkles: add graphql ui generator

--- src/generators/graphql-ui/generator.ts
+++ src/generators/graphql-ui/generator.ts
@@ -1,24 +1,50 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { libraryGenerator } from '@nx/nest';
 import * as path from 'path';
+import { hyphenToCapital } from '../../utils';
+import graphqlMutationGenerator from './mutation/generator';
+import graphqlQueryGenerator from './query/generator';
 import { GraphqlUiGeneratorSchema } from './schema';
 
 export async function graphqlUiGenerator(
   tree: Tree,
   options: GraphqlUiGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  const { name, tags, templatePath } = options;
+  await libraryGenerator(tree, {
+    ...options,
+    name: `${name}-graphql-ui`,
+    directory: `libs/${name}/graphql-ui`,
+    projectNameAndRootFormat: 'as-provided',
+    tags: tags ? `layer:ui,${tags}` : `layer:ui`,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  tree.delete(`libs/${name}/graphql-ui/src/lib`);
+  tree.write(
+    `libs/${name}/graphql-ui/src/index.ts`,
+    `export * from './modules/${name}-graphql-ui.module';`
+  );
+  await graphqlMutationGenerator(tree, {
+    name: `create-${name}`,
+    subDomain: name,
+    skipFormat: true,
+    templatePath: templatePath && `${templatePath}/mutation`,
+  });
+  await graphqlQueryGenerator(tree, {
+    name: `get-${name}`,
+    subDomain: name,
+    skipFormat: true,
+    templatePath: templatePath && `${templatePath}/query`,
+  });
+  generateFiles(
+    tree,
+    (templatePath && `${templatePath}/modules`) ||
+      path.join(__dirname, 'files/modules'),
+    `libs/${name}/graphql-ui/src/modules`,
+    {
+      name,
+      hyphenToCapital,
+    }
+  );
   await formatFiles(tree);
 }
 
